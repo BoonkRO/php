fb7daf3d43e473d18740c7091deed972087f49ea
Guillermo Cirer <guille.mca@hotmail.com>
2017-10-28 14:43:21 +0200

    implementado subida de archivos (txt y json) y documentado codigo

--- practica2/index.php
+++ practica2/index.php
@@ -20,6 +20,9 @@
             background-color: #4CAF50;
             color: white;
         }
+        .error {
+          color: red;
+        }
     </style>
 </head>
 
@@ -27,7 +30,7 @@
   <h3> Arrays precargadas </h3>
 
   <?php
-    $output = "<table><thead><tr><th>Array sin ordenar</th><th>Ordenada por burbuja</th><th>Ordenada por intercambio</th><th>Ordenada por quick sort</th></tr></thead><tbody>";
+    //Inicializamos las arrays precargadas
     $garbage = [
       [1],
       [1,2],
@@ -38,12 +41,18 @@
       [0,0,0,0,0,0,0,0,1,0],
       [1,2,3,4,5,6,7,8,9],
     ];
-
+    //Creamos un array con todos los metodos de ordenacion de los que disponemos
     $sorts = [
       'ordenar_array_por_burbuja',
       'ordenar_array_por_intercambio',
       'ordenar_array_por_quick_sort',
     ];
+    //obtenemos los valores de las variables que vienen por POST
+    $sort = $_POST['metodoOrdenacion']; //de este modo sabemos que metodo tenemos que aplicar
+    if (isset($sort)) {
+      //sabiendo el metodo, formateamos el nombre para posterior uso en el html
+      $methodName=str_replace('_', ' ', $sorts[$sort]);     
+    }
 
     function ordenar_array_por_burbuja($garbage) {
       do {
@@ -80,17 +89,16 @@
     }
 
     function ordenar_array_por_quick_sort($garbage) {
-      // find array size
       $length = count($garbage);
-      // base case test, if array of length 0 then just return array to caller
+      //caso base: si la array es mas pequeña que 0 entonces no hace falta ordenar
       if ($length <= 1) {
         return $garbage;
       } else {
-        // select an item to act as our pivot point, since list is unsorted first position is easiest
+        //seleccionamos un numero como pivote, en este caso el primero que aparece
         $pivot = $garbage[0];
-        // declare our two arrays to act as partitions
+        //declaramos dos arrays que actuan como particiones
         $left = $right = array();
-        // loop and compare each item in the array to the pivot value, place item in appropriate partition
+        //recorremos y comparamos cada numero en la array con el valor del pivote, y lo colocamos en el puesto que corresponde
         for($i = 1; $i < count($garbage); $i++) {
           if($garbage[$i] < $pivot) {
             $left[] = $garbage[$i];
@@ -98,11 +106,15 @@
             $right[] = $garbage[$i];
           }
         }
-        // use recursion to now sort the left and right lists
+        //usamos recursion para ordenar las arrays que quedan a la izquierda y a la derecha
         return array_merge(ordenar_array_por_quick_sort($left), array($pivot), ordenar_array_por_quick_sort($right));
       }
     }
-
+    //construimos una tabla de resultados
+    $output = "<table><thead><tr><th>Array sin ordenar</th><th>Ordenada por burbuja</th><th>Ordenada por intercambio</th><th>Ordenada por quick sort</th></tr></thead><tbody>";
+    /*por cada array precargado mostramos los valores en la primera columna de la tabla
+    por cada metodo en la lista de sorts, añadimos una columna en la que mostraremos el resultado de haber 
+    aplicado dicho sort al array precargado que estemos leyendo en ese momento*/
     foreach ($garbage as $g) {
       $output.='<tr><td>'.implode(', ', $g).'</td>';
         foreach ($sorts as $s) {
@@ -114,64 +126,117 @@
     echo $output;
   ?>
 
-  <hr><h3>Generacion aleatoria de Arrays</h3>
-  <form action="" method="get">
+  <hr><h3>1. Generacion aleatoria de Arrays</h3>
+  <form action="index.php" method="post" enctype="multipart/form-data">
   Cantidad de numeros a generar: <input type="number" name="cantidadNumeros">
-  Elige un método de ordenación: 
+  <br><br>
+
+  <?php
+    //si tenemos la variable por parte del usuario inicializaremos la construccion aleatoria del array
+    if (isset($_POST['cantidadNumeros']) && $_POST['cantidadNumeros'] > 0) {
+      $size = $_POST['cantidadNumeros'];
+      $garbage = [];
+      //creamos tantos numeros como nos ha introducido el usuario, limitamos el tamaño de estos por legibilidad
+      for ($i=0; $i<$size; $i++) {
+        $garbage[$i] = rand(0, 80);
+      }
+      //una vez construido el array aleatorio, lo pasamos por el metodo de sort que se haya elegido y construimos una
+      //tabla de resultados
+      $output = '<table><thead><tr><th>Array generada aleatoriamente</th><th>Despues de '.$methodName.' </th></tr></thead><tbody><tr>';
+      $output.='<td>'.implode(', ', $garbage).'</td>';
+      $sortedArray = $sorts[$sort]($garbage);
+      $output.='<td>'.implode(', ', $sortedArray).'</td>';
+      $output.='</tbody></table>';
+      echo $output;
+    }
+  ?>
+
+  <br><hr><h3>2. Introducir array manualmente</h3>
+  Array (números enteros separados por comas): <input type="text" name="arrayManual">
+  <br><br>
+  <?php
+    if (isset($_POST['arrayManual']) && $_POST['arrayManual'] != "") {
+      //comprobaremos que el input cumple con el formato pedido => ejemplo: 1, 4, 5, 2, 6
+      $input = $_POST['arrayManual'];
+      $input = str_replace(' ', '', $input);
+      $garbage = explode(',', $input);
+      $garbage_are_numbers = true;
+      foreach($garbage as $value) {
+        if (!is_numeric($value)) {
+          $garbage_are_numbers = false;
+        }
+      }
+      //construimos tabla de resultados
+      if ($garbage_are_numbers == true) {
+        $output = '<table><thead><tr><th>Array introducida</th><th>Despues de '.$methodName.' </th></tr></thead><tbody><tr>';
+        $output.='<td>'.implode(', ', $garbage).'</td>';
+        $sortedArray = $sorts[$sort]($garbage);
+        $output.='<td>'.implode(', ', $sortedArray).'</td>';
+        $output.='</tbody></table>';  
+        echo $output;
+      } else {
+        //de no haberse cumplido el formato solicitado avisaremos al usuario
+        $output = '<h4 class="error">Por favor, introduce valores númericos enteros!</h4>';
+        echo $output;
+      }
+    }
+  ?>
+
+  <br><hr><h3>3. Cargar array por archivo</h3>
+  Selecciona archivo (txt, json):  <input type="file" name="archivoSubido" id="archivoSubido">
+  <br><br>
+
+  <?php
+    if (isset($_FILES['archivoSubido']['name']) && $_FILES['archivoSubido']['name'] != "") {
+      //obtenemos la posicion en la que se encuentra el . en el archivo subido
+      $point = strpos($_FILES['archivoSubido']['name'], '.');
+      //una vez tenemos el . leemos el resto del string para saber la extension (.txt o .json soportados)
+      $extension = substr($_FILES['archivoSubido']['name'], $point, strlen($_FILES['archivoSubido']['name']));
+      //cargamos el archivo en memoria
+      $file_content = file_get_contents($_FILES['archivoSubido']['tmp_name']);
+      //construimos la cabecera de la tabla de resultados
+      $output = '<table><thead><tr><th>Array cargada de ['.$_FILES['archivoSubido']['name'].']</th><th>Despues de '.$methodName.' </th></tr></thead><tbody><tr>';
+      if ($extension == '.json') {
+        //si el archivo es un json lo decodificamos (ya que al estar cargado en memoria es un string) para 
+        //poder tratarlo y aplicarle el sort. Finalmente construimos el cuerpo de la tabla de resultado
+        $json_file = json_decode($file_content, true);
+        $output.='<td>'.implode(', ', $json_file['array']).'</td>';
+        $sortedArray = $sorts[$sort]($json_file['array']);
+        $output.='<td>'.implode(', ', $sortedArray).'</td>';
+        $output.='</tbody></table>';  
+        echo $output;
+      }
+      else if ($extension == '.txt') {
+        //si el archivo es un txt lo tratamos (ya que al estar cargado en memoria es un string) para 
+        //poder aplicarle el sort. Finalmente construimos el cuerpo de la tabla de resultado
+        $garbage = explode(', ', $file_content);
+        $output.='<td>'.implode(', ', $garbage).'</td>';
+        $sortedArray = $sorts[$sort]($garbage);
+        $output.='<td>'.implode(', ', $sortedArray).'</td>';
+        $output.='</tbody></table>';  
+        echo $output;        
+      }
+      else {
+        //si el archivo no era ni txt ni json avisamos al usuario
+        echo '<h4 class="error">El formato del archivo no está soportado<h4>';
+      }
+    } 
+  ?>
+
+  <br><br><hr>
+
+  <h3>4. Elige un método de ordenacion</h3>
+  Métodos: 
   <?php 
+    //creamos un select que tendra como opciones cada metodo cargado en la lista de sorts.
+    //el valor que pasaremos por post sera la posicion del metodo en la array (0, 1, 2)
     $output='<select name="metodoOrdenacion">';
     foreach($sorts as $i=>$s) {
       $output.='<option value="'.$i.'">'.str_replace('_', ' ', $s).'</option>';
     }
     echo $output;
   ?>
-
   <input type="submit">
-  <br><br>
-  <?php
-    if (isset($_GET['cantidadNumeros']) && $_GET['cantidadNumeros'] > 0) {
-      $size = $_GET['cantidadNumeros'];
-      $sort = $_GET['metodoOrdenacion'];
-      $garbage = [];
-      for ($i=0; $i<$size; $i++) {
-        $garbage[$i] = rand(0, 80);
-      }
-      $method=str_replace('_', ' ', $sorts[$sort]);
-
-      $output = '<table><thead><tr><th>Array generada aleatoriamente</th><th>Despues de '.$method.' </th></tr></thead><tbody><tr>'; 
-      $output.='<td>'.implode(', ', $garbage).'</td>';
-      $outputSort = $sorts[$sort]($garbage);
-      $output.='<td>'.implode(', ', $outputSort).'</td>';
-      $output.='</tbody></table>';
-      echo $output;
-    }
-  ?>
-  <br><hr><h3>Introducir array manualmente</h3>
-
-  Array: <input type="text" name="arrayManual">
-  <input type="submit">
-  <br><br>
-  <?php
-    if (isset($_GET['arrayManual']) && $_GET['arrayManual'] != "") {
-      $input = $_GET['arrayManual'];
-      $garbage = [2,32,5];
-      $output = '<table><thead><tr><th>Array generada</th><th>Ordenada por burbuja</th><th>Ordenada por intercambio</th><th>Ordenada por quick sort</th></tr></thead><tbody><tr>'; 
-      $output.='<td>'.implode(', ', $garbage).'</td>';
-      $burbuja = ordenar_array_por_burbuja($garbage);
-      $output.='<td>'.implode(', ', $burbuja).'</td>';
-      $intercambio = ordenar_array_por_intercambio($garbage);
-      $output.='<td>'.implode(', ', $intercambio).'</td>';
-      $quick_sort = ordenar_array_por_quick_sort($garbage);
-      $output.='<td>'.implode(', ', $quick_sort).'</td>';
-      $output.='</tbody></table>';
-      echo $output;
-    }
-  ?>
-  <br><hr><h3>Cargar array por archivo</h3>
-
-  Selecciona archivo (txt, json, xml):  <input type="file" name="archivoSubido" id="archivoSubido">
-  <input type="submit">
-  <br><br>
 
   </form>
   <br>
